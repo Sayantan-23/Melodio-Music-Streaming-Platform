6c29b0a08a3bea5c1f3814fb6fa2002e96b7e558
Sayantan <sayantan358@gmail.com>
2023-07-25 23:48:24 +0530

    modal component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Figtree } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={font.className}>
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Modal from "@/components/Modal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Modal title="Test Modal" description="Test description" isOpen onChange={() => { }}>
+        Test children
+      </Modal>
+    </>
+  );
+};
+
+export default ModalProvider;

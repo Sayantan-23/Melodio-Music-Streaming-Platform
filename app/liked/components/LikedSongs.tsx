c88d6d75c917f479136178c6ddbfe9b9477f65ec
Sayantan <sayantan358@gmail.com>
2023-07-27 00:29:20 +0530

    like functionality complete

---
+++ app/liked/components/LikedSongs.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import LikeButton from "@/components/LikeButton"
+import MediaItem from "@/components/MediaItem"
+import { useUser } from "@/hooks/useUser"
+import { Song } from "@/types"
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
+interface LikedSongsProps {
+  songs: Song[]
+}
+
+const LikedSongs: React.FC<LikedSongsProps> = ({ songs }) => {
+  
+  const router = useRouter()
+  const {isLoading, user} = useUser()
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/')
+    }
+  }, [isLoading, user, router])
+  
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">No Liked Songs</div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gap-x-0 w-full">
+          <div className="flex-1">
+            <MediaItem onClick={()=>{}} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default LikedSongs
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -10,6 +10,8 @@ interface SearchProps {
   };
 }
 
+export const revalidate = 0
+
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
 

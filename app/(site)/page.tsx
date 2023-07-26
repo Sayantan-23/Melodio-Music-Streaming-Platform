711b73bf996eaf27a9cbb850486c699feb265da3
Sayantan <sayantan358@gmail.com>
2023-07-26 20:16:31 +0530

    getSongs added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
@@ -8,7 +10,11 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
